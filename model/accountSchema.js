5693565fc71a6a71fd0097456b83665a3e3c0ed1
AureliusLim <aurelius_justin_lim@dlsu.edu.ph>
2023-06-16 13:11:36 +0800

    implement basic login and regis

---
+++ model/accountSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const accountSchema = new mongoose.Schema({
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    profilePhoto: {
+      type: String,
+      required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+  });
+  const Account = mongoose.model(('Account'), accountSchema);
+
+  module.exports = Account;
---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const bcrypt = require('bcrypt');
+const Account = require('./model/accountSchema');
+const path = require('path');
+const app = express();
+const port = 4000;
+
+// Set up the 'hbs' view engine
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname));
+app.use(express.json());
+app.use(fileUpload());
+app.set('views', './frontend');
+mongoose.connect('mongodb://0.0.0.0/cssecdvDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define a route for '/register' to render the registration template
+app.get('/', (req, res) => {
+  res.render('login.hbs');
+});
+// Handle the login form submission
+app.post('/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    try {
+      // Find the user by email
+      const user = await Account.findOne({ email : email });
+        console.log(email)
+      if (user) {
+        // Compare the provided password with the stored hashed password
+        const isMatch = await bcrypt.compare(password, user.password);
+  
+        if (isMatch) {
+          // Passwords match, user is authenticated
+          res.send('Login successful');
+        } else {
+          // Passwords do not match
+          res.send('Invalid credentials');
+        }
+      } else {
+        // User not found
+        res.send('invalid credentials');
+      }
+    } catch (err) {
+      console.log(err);
+      res.send('Error occurred');
+    }
+  });
+
+app.get('/register', (req, res) => {
+  res.render('registration.hbs');
+});
+
+app.post('/registerdetails', async (req, res) => {
+  const profphoto = req.files.profilephoto;
+  const fullname = req.body.fullname;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const password = req.body.password;
+
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const account = await Account.create({
+      fullName: fullname,
+      email: email,
+      phoneNumber: phone,
+      profilePhoto: "images/" + profphoto.name,
+      password: hashedPassword // Store the hashed password in the database
+    });
+
+    const uploadPath = path.join(__dirname, 'images', profphoto.name);
+    profphoto.mv(uploadPath, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("ADDED");
+        console.log(account);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
